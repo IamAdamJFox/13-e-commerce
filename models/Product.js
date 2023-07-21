0f2185d45a5738e48a13556882d94d423825d7c6
adamjfox <adamjeffersonfox@gmail.com>
2023-07-21 07:52:51 -0700

    Models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true, // Validate that the value is an integer
+        min: 0, // Minimum value allowed for stock
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category', // References the 'category' table
+        key: 'id', // References the primary key of the 'category' table
+      },
+    },
   },
   {
     sequelize,
